9e56c40ab61b024519c2625cf0ee9d992e1d2d2c
nayana <shirsatnayana@gmail.com>
2023-02-09 21:54:10 +0530

    to learn

--- src/InterviewPrograms/ArrayListSort.java
+++ src/InterviewPrograms/ArrayListSort.java
@@ -1,20 +1,22 @@
 package InterviewPrograms;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class ArrayListSort {
     public static void main(String[] args) {
-        List<Integer> arraylist=new ArrayList<>();
-        arraylist.add(5);
+     //   List<Integer> arraylist=new ArrayList<>();
+        List<Integer> arraylist= Arrays.asList(5,4,15,500,50,3,11);
+       /* arraylist.add(5);
         arraylist.add(4);
         arraylist.add(15);
         arraylist.add(50);
         arraylist.add(500);
         arraylist.add(3);
         arraylist.add(23);
-        arraylist.add(11);
+        arraylist.add(11)*/
       //  Collections.sort(arraylist,Collections.reverseOrder());
        // Collections.reverse(arraylist);
         Collections.sort(arraylist);
